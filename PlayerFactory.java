29dd58a170b8be46dd6701a886e869d6c1a1c894
Bremworth <thebremworth@gmail.com>
2013-10-14 13:58:50 +1100

    Create PlayerFactory.java

---
+++ PlayerFactory.java
@@ -0,0 +1,18 @@
+/**
+ * Written for COMP9024 2013s2 at UNSW Kensington.
+ * @author dstacey@cse.unsw.edu.au
+ *
+ *  EDIT this file
+ */
+
+package dracula;
+
+import labs.dracula.Dracula;
+
+public class PlayerFactory {
+
+   public static Dracula getDracula(String pastPlays, String[] messages) {
+      return null;
+   }
+
+}
