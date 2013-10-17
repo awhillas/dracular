b2e9a964b0ce1063a210922831c3f3d4016e08b3
munro-no1 <munro_no1@hotmail.com>
2013-10-17 12:54:53 +1100

    Added function getRoute to return route between two cities

--- Map.java
+++ Map.java
@@ -25,4 +25,9 @@ public interface Map {
 	 * Returns true if the location is at sea
 	 */
 	boolean isAtSea(String location);
+	
+	/*
+         * Uses the pathfinder to get Route from one location to another using specified travel type
+         */
+        ArrayList<String> getRoute(String start, String finish, ArrayList<String> avoid, TravelBy by);
 }
