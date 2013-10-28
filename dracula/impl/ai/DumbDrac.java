63e8b16566355848534ee626fad59ef07a6a825c
munro-no1 <munro_no1@hotmail.com>
2013-10-28 18:08:52 +1100

    uses circling ai

---
+++ dracula/impl/ai/DumbDrac.java
@@ -0,0 +1,17 @@
+package dracula.impl.ai.*;
+
+import dracula.*;
+import dracula.impl.ai.*;
+
+/**
+ *
+ * @author JEM
+ */
+public class DumbDrac implements Dracula {
+    private DumbMove move = new DumbMove("CD");
+    
+    public DraculaMove decideMove(){
+        this.move = DracCirclingAI.getDracBestMove(this.move.getPlayAsString());
+        return this.move.getPlayAsString();
+    }
+}
